caddf9aa9098701fa33cfaaed6904a8991620441
pasinsiri <pasin_plp0206@hotmail.com>
2023-09-16 13:23:18 +0700

    add random forest training

--- src/train_random_forest/run.py
+++ src/train_random_forest/run.py
@@ -13,6 +13,7 @@ import json
 
 import pandas as pd
 import numpy as np
+import tempfile
 from sklearn.compose import ColumnTransformer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.impute import SimpleImputer
@@ -54,7 +55,7 @@ def go(args):
     ######################################
     # Use run.use_artifact(...).file() to get the train and validation artifact (args.trainval_artifact)
     # and save the returned path in train_local_pat
-    trainval_local_path = # YOUR CODE HERE
+    trainval_local_path = run.use_artifact(args.trainval_artifact).file()
     ######################################
 
     X = pd.read_csv(trainval_local_path)
@@ -76,6 +77,7 @@ def go(args):
     ######################################
     # Fit the pipeline sk_pipe by calling the .fit method on X_train and y_train
     # YOUR CODE HERE
+    sk_pipe.fit(X_train, y_train)
     ######################################
 
     # Compute r2 and MAE
@@ -98,6 +100,25 @@ def go(args):
     # Save the sk_pipe pipeline as a mlflow.sklearn model in the directory "random_forest_dir"
     # HINT: use mlflow.sklearn.save_model
     # YOUR CODE HERE
+    with tempfile.TemporaryDirectory() as temp_dir:
+        export_path = os.path.join(temp_dir, "random_forest_dir")
+        
+        mlflow.sklearn.save_model(
+            sk_pipe,
+            export_path,
+            serialization_format=mlflow.sklearn.SERIALIZATION_FORMAT_CLOUDPICKLE,
+            input_example=X_val.iloc[:2]
+        )
+
+        artifact = wandb.artifact(
+            args.output_artifact,
+            type='model_export',
+            description='Random Forest pipeline export'
+        )
+        artifact.add_dir(export_path)
+        run.log_artifact(artifact)
+        artifact.wait()
+
     ######################################
 
     ######################################
