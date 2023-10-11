04769e1f4c4850e348d93bb854929e5a863de1ea
pasinsiri <pasin_plp0206@hotmail.com>
2023-10-11 22:21:31 +0700

    refactor test_data

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -44,12 +44,16 @@ def test_proper_boundaries(data: pd.DataFrame):
     """
     Test proper longitude and latitude boundaries for properties in and around NYC
     """
-    idx = data['longitude'].between(-74.25, -73.50) & data['latitude'].between(40.5, 41.2)
+    idx = data['longitude'].between(-74.25, - \
+                                    73.50) & data['latitude'].between(40.5, 41.2)
 
     assert np.sum(~idx) == 0
 
 
-def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_threshold: float):
+def test_similar_neigh_distrib(
+        data: pd.DataFrame,
+        ref_data: pd.DataFrame,
+        kl_threshold: float):
     """
     Apply a threshold on the KL divergence to detect if the distribution of the new data is
     significantly different than that of the reference dataset
@@ -65,6 +69,7 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 ########################################################
 def test_row_count(data):
     assert 15000 < data.shape[0] < 1000000
+
 
 def test_price_range(data, min_price, max_price):
     assert data['price'].between(min_price, max_price)
